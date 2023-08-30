7b53145b5c633a2dee861600d648fd9eb762564e
Satya Dileep Penmetsa <satyadileepvarma@gmail.com>
2023-08-30 09:00:30 -0400

    basic project setup

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+from . import views
+
+urlpatterns = [
+    path("",views.home),
+    path("products/",views.products),
+    path("customers/",views.customers)
+]
---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+# Create your views here.
+
+
+
+def home(request):
+    return render(request, "accounts/dashboard.html")
+
+
+
+def products(request):
+    return HttpResponse("products")
+
+
+def customers(request):
+    return HttpResponse("customers")
+
+
+
