f38acafd94e025739a64d78c98a33565fd911eb2
Satya Dileep Penmetsa <satyadileepvarma@gmail.com>
2023-08-31 13:06:13 -0400

    adding Dynamic urls ,  Python forms &linking page

--- accounts/models.py
+++ accounts/models.py
@@ -22,9 +22,14 @@ class Product(models.Model):
     name =models.CharField(max_length= 200, null= True) 
     price = models.FloatField(null=True)
     category = models.CharField(max_length= 200, null= True , choices= CATEGORY)
-    description = models.CharField(max_length= 200, null= True) 
+    description = models.CharField(max_length= 200, null=True, blank=True) 
     date_cerated = models.DateTimeField(auto_now_add= True, null=True)
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+    
+
 
 
 
@@ -36,10 +41,15 @@ class Order(models.Model):
     STATUS = (
         ("Pending","Pending"),
         ("Out for Delivery", "Out for Delievry"),
-                    ("Delivered","Deliverred")
+                    ("Delivered","Delivered")
     )
     customer = models.ForeignKey(Customer, null= True,on_delete= models.SET_NULL)
     product = models.ForeignKey(Product, null= True , on_delete= models.SET_NULL)
-    date_cerated = models.DateTimeField(auto_now_add= True, null=True)
+    date_created = models.DateTimeField(auto_now_add= True, null=True)
     status = models.CharField(max_length= 200, null= True , choices=STATUS)
+
+
+    def __str__(self):
+     return self.product.name
+    
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,95 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse 
 
+import logging
 # Create your views here.
 
+from .models import*
+
+from .forms import OrderForm
 
 
 def home(request):
-    return render(request, "accounts/dashboard.html")
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    delivered = orders.filter(status="Delivered").count()
+    pending = orders.filter(status="Pending").count()
+    total_orders   = orders.count()
+
+    context = {'orders': orders, 'customers':customers,
+               'total_orders':total_orders,"delivered":delivered,
+               "pending":pending , 
+    } 
+
+    return render(request, "accounts/dashboard.html",context)
 
 
 
 def products(request):
-    return render(request,"accounts/products.html")
-
-
-def customers(request):
-    return render(request,"accounts/customer.html")
+    products = Product.objects.all()
+    
+    return render(request,"accounts/products.html",{"products":products})
 
 
 
+def customers(request, pk):
+    customer = Customer.objects.get(id=pk) 
+
+    orders = customer.order_set.all()
+    logging.info(orders)
+    
+    order_count = orders.count()
+    context = {"customer":customer,"orders":orders, "order_count":order_count}
+
+
+    return render(request,"accounts/customer.html", context)
+
+
+
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method =="POST":
+        # print(request.POST)
+        form = OrderForm(request.POST)
+
+        if form.isvalid():
+            form.save()
+            return redirect("/")
+
+
+    context = {"form":form}
+    return render(request, "accounts/order_form.html", context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method =="POST":
+      # print(request.POST)
+      form = OrderForm(request.POST, instance=order)
+
+      if form.isvalid():
+          form.save()
+          return redirect("/")
+
+    context = {"form":form}
+
+    return render(request,"accounts/order_form.html",context) 
+
+
+
+
+def deleteOrder(request,pk):
+    
+    order = Order.objects.get(id=pk)
+
+    if request.method =="POST":
+        order.delete()
+        return redirect("/")
+         
+    context ={"item":order}
+
+    return render(request,"accounts/delete.html", context)
