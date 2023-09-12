7a8eadc7e2287faca9e9cf11a955f5f8113fb424
Satya Dileep Penmetsa <satyadileepvarma@gmail.com>
2023-09-12 09:16:10 -0400

    adding authentication and authorization

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 from .import views
 
 urlpatterns = [
+    path("register/",views.registerPage,name ="register"),
+    path("login/", views.loginPage, name = "login"),
+        path("logout/", views.logoutPage, name = "logout"),
     path("",views.home, name ="home"),
     path("products/",views.products, name ="products"),
 
