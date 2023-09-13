99c5f9b7346a1bd2e50b9d4aa691601b2a735168
Satya Dileep Penmetsa <satyadileepvarma@gmail.com>
2023-09-13 10:50:34 -0400

    .

--- crm1/settings.py
+++ crm1/settings.py
@@ -132,11 +132,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #SMTP CONFIG
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST ="smtp.gmail.com"
-EMAIL_PORT  ="587"
-EMAIL_HOST_USER ="satyadileep.projects@gmail.com"
-EMAIL_HOST_PASSWORD = "woamtvkpmefcwrcj"
-EMAIL_USE_TLS= True
-EMAIL_USE_ssl = False
